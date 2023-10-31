8e2f57b80a51ceae3c7b220b228fbe300fb648ad
j-lazo <j.org.ls@ciencias.unam.mx>
2023-10-31 12:14:28 +0100

    typo FLAGAS corrected

--- run_training.sh
+++ run_training.sh
@@ -1,2 +1,2 @@
 PATH_DATASET_DIR=''
-python main.py --name_model=simple_classifier --path_dataset=$PATH_DATASET_DIR --type_training=custom_training --epochs=20 --learning_rate=0.00001 --backbone=resnet101
+python main.py --name_model=simple_classifier --path_train_dataset=$PATH_DATASET_DIR --type_training=custom_training --epochs=20 --learning_rate=0.00001 --backbone=resnet101
